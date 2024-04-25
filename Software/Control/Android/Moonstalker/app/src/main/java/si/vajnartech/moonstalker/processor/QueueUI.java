f2a8e3081ccad6ecb03a485fcf9782b74330c746
Zoran Robic <vajnar7@gmail.com>
2024-04-25 10:42:47 +0200

    Changes for 3.0 version

--- Software/Control/Android/Moonstalker/app/src/main/java/si/vajnartech/moonstalker/processor/QueueUI.java
+++ Software/Control/Android/Moonstalker/app/src/main/java/si/vajnartech/moonstalker/processor/QueueUI.java
@@ -3,6 +3,8 @@ package si.vajnartech.moonstalker.processor;
 import static si.vajnartech.moonstalker.C.CALIBRATOR;
 import static si.vajnartech.moonstalker.C.ST_ASTRO_DATA;
 import static si.vajnartech.moonstalker.C.ST_BATTERY;
+import static si.vajnartech.moonstalker.C.ST_CALIBRATED;
+import static si.vajnartech.moonstalker.C.ST_CALIBRATING;
 import static si.vajnartech.moonstalker.C.ST_CONNECTION_ERROR;
 import static si.vajnartech.moonstalker.C.ST_ERROR;
 import static si.vajnartech.moonstalker.C.ST_INFO;
@@ -15,12 +17,17 @@ import static si.vajnartech.moonstalker.C.ST_WARNING;
 import static si.vajnartech.moonstalker.OpCodes.MSG_BATTERY;
 import static si.vajnartech.moonstalker.OpCodes.MSG_BATTERY_RES;
 import static si.vajnartech.moonstalker.OpCodes.MSG_CALIBRATED;
+import static si.vajnartech.moonstalker.OpCodes.MSG_CALIBRATING;
 import static si.vajnartech.moonstalker.OpCodes.MSG_CONNECT;
 import static si.vajnartech.moonstalker.OpCodes.MSG_CONN_ERROR;
 import static si.vajnartech.moonstalker.OpCodes.MSG_ERROR;
 import static si.vajnartech.moonstalker.OpCodes.MSG_GET_ASTRO_DATA;
 import static si.vajnartech.moonstalker.OpCodes.MSG_INFO;
 import static si.vajnartech.moonstalker.OpCodes.MSG_MOVE;
+import static si.vajnartech.moonstalker.OpCodes.MSG_MOVE_END;
+import static si.vajnartech.moonstalker.OpCodes.MSG_MOVE_START;
+import static si.vajnartech.moonstalker.OpCodes.MSG_MVE_ACK;
+import static si.vajnartech.moonstalker.OpCodes.MSG_MVS_ACK;
 import static si.vajnartech.moonstalker.OpCodes.MSG_MV_ACK;
 import static si.vajnartech.moonstalker.OpCodes.MSG_NOT_READY;
 import static si.vajnartech.moonstalker.OpCodes.MSG_PING;
@@ -30,6 +37,9 @@ import static si.vajnartech.moonstalker.OpCodes.MSG_WARNING;
 
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
+
+import java.util.concurrent.CountDownLatch;
 
 import si.vajnartech.moonstalker.telescope.Actions;
 
@@ -52,14 +62,12 @@ public class QueueUI extends Handler
         } else if (message.what == MSG_BATTERY) {
             new CmdBattery(this);
             actions.updateStatus(ST_WAITING);
-        } else if (message.what == MSG_PING) {
-            new CmdPing(this);
-            actions.updateStatus(ST_WAITING);
         } else if (message.what == MSG_MOVE) {
             String obj = (String) message.obj;
             new CmdMove(this, obj);
             actions.updateStatus(ST_WAITING);
         } else if (message.what == MSG_CALIBRATED) {
+            actions.updateMode(ST_CALIBRATED);
             String obj = (String) message.obj;
             new CmdCalibrated(this, obj);
         } else if (message.what == MSG_GET_ASTRO_DATA) {
@@ -87,6 +95,19 @@ public class QueueUI extends Handler
         } else if (message.what == MSG_POS) {
             String msg = (String) message.obj;
             actions.updateStatus(ST_POS, msg);
+        } else if (message.what == MSG_CALIBRATING) {
+            actions.updateMode(ST_CALIBRATING);
+        } else if (message.what == MSG_MOVE_START) {
+            String obj = (String) message.obj;
+            new CmdMoveStart(this, obj);
+            actions.updateStatus(ST_WAITING);
+        } else if (message.what == MSG_MOVE_END) {
+            new CmdMoveEnd(this);
+            actions.updateStatus(ST_WAITING);
+        } else if (message.what == MSG_MVS_ACK) {
+            // todo
+        } else if (message.what == MSG_MVE_ACK) {
+            // todo
         }
     }
 }
